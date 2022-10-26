7bcdc8c94fb54a9b57904c264b3c021700a5ceab
AdyArkPra <adyatmaarkaanpradipa@gmail.com>
2022-10-26 18:11:10 +0700

    TS - Day 1

--- Adyatma.ts
+++ Adyatma.ts
@@ -6,7 +6,6 @@
 function pemisah(text: string, index1: number, index2: number): string{
     return text.slice(index1, index2);
 }
-
 const text = "Learning Typescript is different than Javascript";
 const index1 = 9;
 const index2 = 19;
