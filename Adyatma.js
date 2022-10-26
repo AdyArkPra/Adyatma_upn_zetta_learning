aa33385417a850fa8a0135fc14d5626b60c12867
AdyArkPra <adyatmaarkaanpradipa@gmail.com>
2022-10-26 18:06:59 +0700

    TS - Day 1

---
+++ Adyatma.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} text
+ * @param {Number} index1
+ * @param {Number} index2
+ */
+function pemisah(text, index1, index2) {
+    return text.slice(index1, index2);
+}
+const text = "Learning Typescript is different than Javascript";
+const index1 = 9;
+const index2 = 19;
+console.log(pemisah(text, index1, index2));
+//# sourceMappingURL=Adyatma.js.map
---
+++ Adyatma.ts
@@ -0,0 +1,14 @@
+/**
+ * @param {string} text
+ * @param {Number} index1
+ * @param {Number} index2
+ */
+function pemisah(text: string, index1: number, index2: number): string{
+    return text.slice(index1, index2);
+}
+
+const text = "Learning Typescript is different than Javascript";
+const index1 = 9;
+const index2 = 19;
+
+console.log(pemisah(text, index1, index2));
